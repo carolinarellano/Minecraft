ece324a4c47b2df6f5d0643c2666f5f9175e23bb
Carolina Arellano <carolina.arellanov02@gmail.com>
2023-05-02 14:11:40 -0600

    Re-configured the project

--- src/main/java/com/rcy/elements/InitArmor.java
+++
@@ -1,8 +0,0 @@
-package com.rcy.elements;
-
-import net.minecraft.world.item.Item;
-import net.minecraftforge.registries.DeferredRegister;
-
-public class InitArmor {
-    //public static final DeferredRegister<Item>;
-}
